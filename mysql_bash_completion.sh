ec082d364ec565b29776bf6825167a345e1b3a48
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2015-04-02 23:01:24 -0300

    Autocomplete double dash options

--- mysql_bash_completion.sh
+++ mysql_bash_completion.sh
@@ -3,12 +3,14 @@ _mysql()
     local prefix=${COMP_WORDS[COMP_CWORD]}
     local options=''
 
-    if [ "$prefix" == "-" ]
-    then
-      options=$(mysql --help | sed -n -e '/^[ ][ ]*-[^-]/ { s/,.*$//; p; }' | xargs)
-    else
-      options=$(mysql -uroot test -e 'show databases' | sed -n '2,$ p' | xargs)
-    fi
+    # Get dash options
+    options=$(mysql --help | sed -n -e '/^[ ][ ]*-[^-]/ { s/,.*$//; p; }' | xargs)
+
+    # Get double dash options
+    options="$options $(mysql --help | sed -n 's/^[ ]*//; /^-/ {; s/^-[a-zA-Z#?],*[ ]*//; s/[ ].*$//; s/=.*$/=/; y/()./   /; s/\[//; p; }' | sort | uniq | sed -n '2,$ p')"
+
+    # Get database names
+    options="$options $(mysql -uroot test -e 'show databases' | sed -n '2,$ p' | xargs)"
 
     COMPREPLY=( $(compgen -W "$options" -- $prefix) )
 }
